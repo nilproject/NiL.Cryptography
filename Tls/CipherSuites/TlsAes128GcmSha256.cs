878f754cfd9e86a1d89c803e059e4049d968cac6
Полхович Дмитрий <po.mithril@gmail.com>
2026-06-06 22:23:52 +0300

    Implementation of TLS_AES_128_GCM_SHA256 with native dotnet AesGcm implementation

--- Tls/CipherSuites/TlsAes128GcmSha256.cs
+++ Tls/CipherSuites/TlsAes128GcmSha256.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Specialized;
+using System.Security.Cryptography;
 using NiL.Cryptography.EllipticCryptography;
 using NiL.Cryptography.EllipticCryptography.WeierstrassForm.Signature;
 using NiL.Cryptography.Encryption.Modes.Gcm;
@@ -133,6 +133,129 @@ public class TlsAes128GcmSha256 : EcdheEcdsaAes128GcmSha256
                 input,
                 outputBuffer[..input.Length],
                 outputBuffer[input.Length..]);
+
+            return outputBuffer;
+        }
+    }
+}
+
+[CipherSuiteId(CipherSuiteId.TLS_AES_128_GCM_SHA256)]
+public class SystemTlsAes128GcmSha256 : EcdheEcdsaAes128GcmSha256
+{
+    public SystemTlsAes128GcmSha256(EcdhKeyDerivation keyDerivationAlgorithm, WeierstrassEcdsa ecdsa) : base(keyDerivationAlgorithm, ecdsa)
+    {
+        PseudoRandomFunction = null;
+        KeyScheduleDerivation = new KeyScheduleDerivation(Hmac, 16, 12);
+    }
+
+    public override CipherSuiteId CipherSuiteId => CipherSuiteId.TLS_AES_128_GCM_SHA256;
+
+    public override TlsVersion[] TlsVersions { get; } = [TlsVersion.Tls13];
+
+    public override IEncryptDecryptProcessor CreateEncryptDecryptPair(KeysSet12 keysSet, TlsVersion tlsVersion)
+    {
+        return new EncryptDecryptProcessor(
+            tlsVersion,
+            keysSet,
+            new AesGcm(keysSet.OurWriteKey),
+            new AesGcm(keysSet.TheirWriteKey),
+            Hmac);
+    }
+
+    private sealed class EncryptDecryptProcessor : IEncryptDecryptProcessor
+    {
+        private const int _nonceLen = 12;
+        private const int _authTagLen = 16;
+
+        private readonly KeysSet12 _keysSet;
+        private readonly AesGcm _outputCipher;
+        private readonly AesGcm _inputCipher;
+        private readonly TlsVersion _tlsVersion;
+        private readonly Hmac _hmac;
+        private ulong _outputSeqNumber;
+        private ulong _inputSeqNumber;
+
+        public EncryptDecryptProcessor(TlsVersion tlsVersion, KeysSet12 keysSet, AesGcm outputCipher, AesGcm inputCipher, Hmac hmac)
+        {
+            if (keysSet.OurWriteKey is null || keysSet.TheirWriteKey is null
+                    || keysSet.OurWriteIV is null || keysSet.TheirWriteIV is null)
+                throw new ArgumentNullException();
+
+            _keysSet = keysSet;
+            _outputCipher = outputCipher ?? throw new ArgumentNullException(nameof(outputCipher));
+            _inputCipher = inputCipher ?? throw new ArgumentNullException(nameof(inputCipher));
+            _hmac = hmac;
+            _tlsVersion = tlsVersion;
+        }
+
+        public unsafe ArraySegment<byte> Decrypt(in ReadOnlySpan<byte> input, TlsContentType tlsContentType)
+        {
+            // https://datatracker.ietf.org/doc/html/rfc8446#section-5.2
+            // TLSInnerPlaintext
+            var realDataLen = input.Length - _authTagLen;
+            var outputBuffer = new BigEndianWriteBuffer(Math.Max(5, realDataLen), true);
+            outputBuffer.Length = realDataLen;
+
+            // additional_data
+            outputBuffer.Uint8((byte)TlsContentType.ApplicationData);
+            outputBuffer.Uint16((ushort)TlsVersion.Tls12);
+            outputBuffer.Uint16((ushort)input.Length);
+
+            var nonce = stackalloc byte[_nonceLen];
+            *(ulong*)nonce = _inputSeqNumber++;
+            *(int*)(nonce + sizeof(ulong)) = 0;
+            for (var i = 0; i < _nonceLen / 2; i++)
+            {
+                var t = nonce[i];
+                nonce[i] = nonce[_nonceLen - 1 - i];
+                nonce[_nonceLen - 1 - i] = t;
+            }
+
+            for (var i = 0; i < _keysSet.TheirWriteIV.Length; i++)
+                nonce[i] ^= _keysSet.TheirWriteIV[i];
+
+            _inputCipher.Decrypt(
+                new ReadOnlySpan<byte>(nonce, _nonceLen),
+                input[..realDataLen],
+                input.Slice(realDataLen, 12),
+                outputBuffer[..realDataLen],
+                outputBuffer.Buffer[..5]);
+
+            outputBuffer.Length = realDataLen;
+            return outputBuffer;
+        }
+
+        public unsafe ArraySegment<byte> Encrypt(in ReadOnlySpan<byte> input, TlsContentType tlsContentType)
+        {
+            // https://datatracker.ietf.org/doc/html/rfc8446#section-5.2
+            // TLSInnerPlaintext
+            var outputBuffer = new BigEndianWriteBuffer(input.Length + _authTagLen, true);
+            outputBuffer.Length = input.Length + _authTagLen;
+
+            // additional_data
+            outputBuffer.Uint8((byte)TlsContentType.ApplicationData);
+            outputBuffer.Uint16((ushort)TlsVersion.Tls12);
+            outputBuffer.Uint16((ushort)(input.Length + _authTagLen));
+
+            var nonce = stackalloc byte[_nonceLen];
+            *(ulong*)nonce = _outputSeqNumber++;
+            *(int*)(nonce + sizeof(ulong)) = 0;
+            for (var i = 0; i < _nonceLen / 2; i++)
+            {
+                var t = nonce[i];
+                nonce[i] = nonce[_nonceLen - 1 - i];
+                nonce[_nonceLen - 1 - i] = t;
+            }
+
+            for (var i = 0; i < _keysSet.OurWriteIV.Length; i++)
+                nonce[i] ^= _keysSet.OurWriteIV[i];
+
+            _outputCipher.Encrypt(
+                new ReadOnlySpan<byte>(nonce, _nonceLen),
+                input,
+                outputBuffer[..input.Length],
+                outputBuffer[input.Length..],
+                outputBuffer[..5]);
 
             return outputBuffer;
         }
